10f0047396d7ec08a27094b90ca1977217d081f6
Fedor Tumanov <fatumx5@mail.ru>
2018-08-08 19:07:32 +0700

    added input validation

--- JS/index.js
+++ JS/index.js
@@ -5,8 +5,7 @@ var mapTool = {
   DOM_tool = {
     JSON_Search: [],
     form: document.forms[0],
-    content: [],
-    responseCount: []
+    content: []
   };
 
 window.onload = init();
@@ -25,25 +24,23 @@ window.onload = init();
 DOM_tool.form.addEventListener('submit', async function(e) {
   var searchLine = DOM_tool.form.elements.findLine.value;
   e.preventDefault();
-  if (DOM_tool.form.elements.findLine.value != "") {
-    showSearchResults(searchLine);
+  if (searchLine != "") {
+    if (searchLine.match(/<.+>/g) === null) {
+      showSearchResults(searchLine);
+    } else {
+      console.log("Invalid input");
+    };
   };
+  DOM_tool.form.elements.findLine.value = "";
 });
 
 function showSearchResults(searchLine) {
-  HTML_str = ''
+  let HTML_str = ''
   Search(searchLine).then(addNew => {
-    DOM_tool.responseCount.push(DOM_tool.JSON_Search.length)
-    DOM_tool.content.push(searchLine);
+    DOM_tool.content.push('<div class = "search-item" onclick="runSearchBy_DivClick(this)"><p class ="recent-text">' +
+      searchLine + '</p>' + '<p class="count">' + DOM_tool.JSON_Search.length + '</p></div>');
 
-    for (i = 0; i < DOM_tool.content.length; i++) {
-      HTML_str = HTML_str +
-        '<div class = "search-item" onclick="runSearchBy_DivClick(this.innerHTML)"><p class ="recent-text">' +
-        DOM_tool.content[i] +
-        '</p>' + '<p class="count">' +
-        DOM_tool.responseCount[i] +
-        '</p></div>';
-    }
+    HTML_str = DOM_tool.content.join('');
     document.getElementById('recent').innerHTML = HTML_str;
     document.getElementsByClassName('search-item')[DOM_tool.content.length - 1].style.backgroundColor = 'rgb(210, 100, 60)';
   });
@@ -53,18 +50,22 @@ function showSearchResults(searchLine) {
 //-------UpdateMarkerState-------
 async function UpdateMarkerState(data) {
   mapTool.group.clearLayers();
+
   if (data[0] != null) {
+
+
     let minX = DG.marker([data[0].lat, data[0].lon]).options.icon.options.iconSize[0];
     let minY = DG.marker([data[0].lat, data[0].lon]).options.icon.options.iconSize[1];
 
     for (j = 0; j < data.length; j++) {
+
       let current = DG.marker([data[j].lat, data[j].lon]).getLatLng();
-      let X1 = mapTool.myMap.latLngToContainerPoint(current).x;
-      let Y1 = mapTool.myMap.latLngToContainerPoint(current).y;
+      X1 = mapTool.myMap.latLngToContainerPoint(current).x;
+      Y1 = mapTool.myMap.latLngToContainerPoint(current).y;
 
       if ((X1 > 100 && X1 < window.innerWidth - 100) && (Y1 > 100 && Y1 < window.innerHeight - 100)) {
-        isShowable = true;
-        isEmpty = false;
+        let isShowable = true;
+        let isEmpty = false;
         for (i = 0; i < data.length; i++) {
           if (j != i) {
             let checking = DG.marker([data[i].lat, data[i].lon]).getLatLng();
@@ -74,9 +75,8 @@ async function UpdateMarkerState(data) {
             if (!(Math.abs(X1 - X2) > minX || Math.abs(Y1 - Y2) > minY)) {
               if (i > j) {
                 isEmpty = true;
-              } else {
-                isShowable = false;
               }
+              isShowable = false;
               break;
 
             };
@@ -99,13 +99,16 @@ async function SetSize(width, height) {
   mapArea.style.width = width + "px";
 };
 
-function runSearchBy_DivClick(line) {
-  searchLine = line.replace(/<p class="recent-text">/, "").replace(/<\/p><p class="count">\d*<\/p>/, "");
-  showSearchResults(searchLine);
-
+function runSearchBy_DivClick(div) {
+  let recentText = div.getElementsByClassName('recent-text')[0].textContent;
+  if (recentText.match(/<.+>/g) === null) {
+    showSearchResults(recentText);
+  } else {
+    console.log("Invalid input");
+  };
 }
 async function Search(searchLine) {
-  searchURL = 'http://catalog.api.2gis.ru/2.0/catalog/marker/search?q=' + searchLine + '&page_size=1000&region_id=32&key=ruhebf8058'
+  let searchURL = 'http://catalog.api.2gis.ru/2.0/catalog/marker/search?q=' + searchLine + '&page_size=3000&region_id=32&key=ruhebf8058'
   await axios.get(searchURL).then(response => {
     DG.then(function() {
       if (response.data.result != null) {
